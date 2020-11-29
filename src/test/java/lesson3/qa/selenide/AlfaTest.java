599dba311eacb32e43a2b9c256b3a28fb7b9ab53
senya0ne <liketravelin@gmail.com>
2020-11-30 00:00:52 +0300

    add tests

---
+++ src/test/java/lesson3/qa/selenide/AlfaTest.java
@@ -0,0 +1,33 @@
+package lesson3.qa.selenide;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class AlfaTest {
+    @Test
+    void archiveDepositsSizeTest() {
+        open("https://alfabank.ru/make-money/");
+
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $$x("//*[@data-test-id='button']").find(text("Архивные счета и депозиты")).click();
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $$x("//*[@data-widget-name='CatalogCard']").shouldHave(CollectionCondition.size(5));
+    }
+
+    @Test
+    void InsuranceSizeTest() {
+        open("https://alfabank.ru/make-money/");
+
+        $(byText("Страхование вкладов")).parent().click();
+        $x("//*[@data-test-id='accordion-header-0']").shouldHave(text("Альфа-Банк является участником системы обязательного страхования вкладов")).click();
+        $x("//*[@data-test-id='accordion-header-1']").shouldHave(text("Федеральный закон от 23.12.2003 N 177-ФЗ «О страховании вкладов физических лиц в банках Российской Федерации»")).click();
+        $x("//*[@data-test-id='accordion-header-2']").shouldHave(text("Страхованию подлежат")).click();
+        $x("//*[@data-test-id='accordion-header-3']").shouldHave(text("Как происходит возмещение средств?")).click();
+    }
+}
